63001f93cefe88bb3db22bd8a315d8ecdc1eb95a
Mathias Bragagia <mathias.bragagia@epitech.eu>
2017-01-20 14:07:53 +0100

    Adds WP_CACHE constant

--- wp-config.php
+++ wp-config.php
@@ -90,5 +90,7 @@ define('WP_DEBUG', false);
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
+define('WP_CACHE', true);
+
 /** Réglage des variables de WordPress et de ses fichiers inclus. */
 require_once(ABSPATH . 'wp-settings.php');
